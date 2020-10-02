e20ae1b3a60f85e0e2763618fe59e6a24b896cd7
Gabriel Siqueira <espaguetetv.sbs@gmail.com>
2020-10-02 20:10:09 -0300

    Inicialização do Repositório

---
+++ src/br/com/siqueira/javacore/introducaometodos/classes/EstudanteV2.java
@@ -0,0 +1,69 @@
+package br.com.siqueira.javacore.introducaometodos.classes;
+
+public class EstudanteV2 {
+    private String nome;
+    private int idade;
+    private double[] notas;
+
+    public void print() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.print("Notas: ");
+
+        if (this.notas != null) {
+            for (int i = 0; i < this.notas.length; i++) {
+                System.out.printf("%.2f ", this.notas[i]);
+            }
+        } else {
+            System.out.println("Não foi informado nenhuma nota");
+        }
+
+    }
+
+    public void tirarMedia() {
+        if (this.notas != null) {
+            double media = 0;
+            for (double valor : this.notas) {
+                media += valor;
+            }
+            media = media / notas.length;
+
+            System.out.printf("\n A média do aluno foi %.2f e ele está ", media);
+            if (media < 7) {
+                System.out.println("Reprovado");
+            } else {
+                System.out.println("Aprovado");
+            }
+        } else {
+            System.out.println("[ERRO] Notas não informadas");
+        }
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        if(idade > 0){
+            this.idade = idade;
+        }else{
+            System.out.println("Você não pode fazer isso!");
+        }
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public double[] getNotas() {
+        return this.notas;
+    }
+}
---
+++ src/br/com/siqueira/javacore/introducaometodos/teste/EstudanteTeste2.java
@@ -0,0 +1,19 @@
+package br.com.siqueira.javacore.introducaometodos.teste;
+
+import br.com.siqueira.javacore.introducaometodos.classes.EstudanteV2;
+
+public class EstudanteTeste2 {
+    public static void main(String[] args) {
+
+        EstudanteV2 discente = new EstudanteV2();
+
+        discente.setNome("Gabriel Siqueira");
+        discente.setIdade(-1);
+        discente.setNotas(new double[]{3, 9, 8});;
+
+        discente.print();
+        discente.tirarMedia();
+        System.out.println(discente.getNome());
+        System.out.println(discente.getIdade());
+    }
+}
