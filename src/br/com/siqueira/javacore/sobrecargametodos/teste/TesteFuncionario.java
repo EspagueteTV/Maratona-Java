da5b5414c37c1a8a5ed2d28f8e7910815bad9727
Gabriel Siqueira <espaguetetv.sbs@gmail.com>
2020-10-31 17:00:52 -0300

    Aula sobre construtores

--- src/br/com/siqueira/javacore/sobrecargametodos/teste/TesteFuncionario.java
+++ src/br/com/siqueira/javacore/sobrecargametodos/teste/TesteFuncionario.java
@@ -6,9 +6,10 @@ public class TesteFuncionario {
 
     public static void main(String[] args) {
 
-        Funcionario funcionario = new Funcionario();
-        funcionario.init("Chica da Silva", "111-222-333-44", 4500, "1222-2");
-        funcionario.imprimi();
+        Funcionario funcionario = new Funcionario("Chica da Silva", "111-222-333-44", 4500, "1222-2");
+        Funcionario funcionario2 = new Funcionario();
 
+        funcionario.imprimi();
+        funcionario2.imprimi();
     }
 }
